4527957354c6f89697532bf899ea53a78c15f9b1
Vignesh <107236186+Vignesxh18@users.noreply.github.com>
2025-10-29 21:14:52 +0530

    Update materials.js

--- src/routes/materials.js
+++ src/routes/materials.js
@@ -1,1 +1,20 @@
+const express = require('express');
+const router = express.Router();
+const db = require('../db');
+
+router.get('/', async (req, res) => {
+  const r = await db.query('SELECT * FROM materials ORDER BY id');
+  res.json(r.rows);
+});
+
+router.post('/', async (req, res) => {
+  const { code, name, unit, hsn_code, gst_percent } = req.body;
+  const r = await db.query(
+    `INSERT INTO materials (code,name,unit,hsn_code,gst_percent) VALUES($1,$2,$3,$4,$5) RETURNING *`,
+    [code,name,unit,hsn_code,gst_percent]
+  );
+  res.json(r.rows[0]);
+});
+
+module.exports = router;
 
